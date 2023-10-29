0504beef33b60b1798e0fdf105409908d40700ae
Matthew Morgan <howcouldyouforgetthisusername@gmail.com>
2023-10-29 18:34:37 -0500

    feat: add broken book, chapter, verse dropdowns

--- bible-guesser-frontend/src/App.js
+++ bible-guesser-frontend/src/App.js
@@ -1,30 +1,110 @@
 import React, { useState } from 'react';
 import './App.css';
+import bibleData from './bibleData';
+
+// Example static data for books, chapters, and verses
+const books = ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy']; // Add the rest of the books
+const chapters = Array.from({ length: 50 }, (_, i) => i + 1); // Example for 50 chapters
+const verses = Array.from({ length: 31 }, (_, i) => i + 1); // Example for 31 verses
 
 function App() {
-  const [verse, setVerse] = useState('Random verse will appear here.');
+  const [book, setBook] = useState('');
+  const [chapter, setChapter] = useState('');
+  const [verse, setVerse] = useState('');
+  const [randomVerse, setRandomVerse] = useState('Random verse will appear here.');
 
-  // This function could be used to get the verse from your Flask API
-  const fetchVerse = () => {
+  const getChapters = (book) => {
+    return Object.keys(bibleData[book].chapters).map(Number);
+  };
+
+  const getVerses = (book, chapter) => {
+    return Array.from({ length: bibleData[book].chapters[chapter] }, (_, i) => i + 1);
+  };
+
+  const handleBookChange = (event) => {
+    const newBook = event.target.value;
+    setBook(newBook);
+    setChapter('');
+    setVerse('');
+  };
+
+  const handleChapterChange = (event) => {
+    const newChapter = event.target.value;
+    setChapter(newChapter);
+    setVerse('');
+  };
+
+  const handleVerseChange = (event) => {
+    setVerse(event.target.value);
+  };
+
+  // Function to fetch a random verse from your backend
+  const fetchRandomVerse = () => {
     fetch('http://localhost:5000/api/random_verse')
       .then(response => response.json())
       .then(data => {
         console.log('Received data:', data); // Log the received data for inspection
-        setVerse(data.verse_text); // Assuming the response has a 'text' field with the verse
+        setRandomVerse(data.verse_text); // Update the state with the fetched verse text
       })
       .catch(error => {
         console.error('Error fetching verse:', error);
       });
   };
 
+  const submitGuess = () => {
+    // Here you would take the selected book, chapter, and verseNumber and send them to the backend to check the guess
+    // For example:
+    // fetch('http://localhost:5000/api/check_guess', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({ book, chapter, verseNumber }),
+    // })
+    // .then(response => response.json())
+    // .then(data => {
+    //   // Handle the response data - whether the guess was correct or not
+    // })
+    // .catch(error => {
+    //   console.error('Error submitting guess:', error);
+    // });
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>BibleGuesser</h1>
-        <button onClick={fetchVerse}>Get Random Verse</button>
-        <p>{verse}</p>
-        {/* Add more UI components here as needed */}
+        {/* Random Verse Section */}
+        <button onClick={fetchRandomVerse}>Get Random Verse</button>
+        <p>{randomVerse}</p>
+        {/* Book Selection */}
+        <select value={book} onChange={handleBookChange}>
+          <option value="">Select Book</option>
+          {Object.keys(bibleData).map((bookName) => (
+            <option key={bookName} value={bookName}>{bookName}</option>
+          ))}
+        </select>
+
+        {/* Chapter Selection */}
+        {book && (
+          <select value={chapter} onChange={handleChapterChange}>
+            <option value="">Select Chapter</option>
+            {getChapters(book).map((chapterNumber) => (
+              <option key={chapterNumber} value={chapterNumber}>{chapterNumber}</option>
+            ))}
+          </select>
+        )}
+
+        {/* Verse Selection */}
+        {chapter && (
+          <select value={verse} onChange={handleVerseChange}>
+            <option value="">Select Verse</option>
+            {getVerses(book, chapter).map((verseNumber) => (
+              <option key={verseNumber} value={verseNumber}>{verseNumber}</option>
+            ))}
+          </select>
+        )}
+        <button onClick={submitGuess}>Submit Guess</button>
       </header>
     </div>
   );
---
+++ bible-guesser-frontend/src/bibleData.js
@@ -0,0 +1,18 @@
+const bibleData = {
+    Genesis: {
+        chapters: {
+            1: 5,
+            2: 7,
+            3: 10
+        }
+    },
+    Exodus: {
+        chapters: {
+            1: 5,
+            2: 7,
+            3: 10
+        }
+    }
+};
+
+export default bibleData;
